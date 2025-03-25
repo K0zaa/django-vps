28f41680d9fb14713974032707341659499f8dae
K0zaa <goatw7806gaming@gmail.com>
2025-03-25 09:20:16 +0100

    .

--- mujprojekt/web/views.py
+++ mujprojekt/web/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Ahoj, tohle je můj první Django projekt!")
+    return HttpResponse("Coming soon")
